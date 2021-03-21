544a5d48ddcecc53688583f906b6a41c572737d1
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2021-03-21 17:39:16 -0600

    Se añade estructura ajax para solicitar se procese comando, respuesta se agrega a historial de comandos

--- sistemas-operativos-avanzados/proyecto-final/js/scripts.js
+++ sistemas-operativos-avanzados/proyecto-final/js/scripts.js
@@ -1,5 +1,64 @@
+let usernameInput = $('#username-input');
+let pathInput = $('#path-input');
+let userInput = $('input#user-input');
+let history = $('.history');
+
 $(document).ready(function() {
     $(".terminal").on('click', function() {
-	$(".user-input").focus();
+	userInput.focus();
+    });
+
+    userInput.keypress(function (e) {
+	var key = e.which;
+	if(key == 13)  // Enter
+	{
+	    if (userInput.val() == 'clear') {
+		history.empty();
+	    } else {
+		send_command();
+	    }
+
+	    // Se resetea comando de usuario
+	    $("#user-input").val('');
+	}
     });
 });
+
+function send_command()
+{
+    let serverResponse = '';
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY',
+        data: {},
+	dataType: 'json',
+        success: function (response) {
+	    console.log('RESPUESTA-GOOGLE:');
+	    console.log(response);
+            if (response) {
+		
+            } else {
+		
+            }
+	    serverResponse = 'Respuesta de servidor';
+        },
+        error: function (error) {
+	    console.log(error);
+            alert('Ocurrio un error al procesar la informacion.');
+        },
+        complete: function () {
+            console.log('Solicitud completa');
+        }
+    });
+
+    // Se agrega a historial
+    history.append(
+	$('<div/>', {class: 'history-row'}).append(
+	    $('<span/>', {text: usernameInput.text() + ' ', class: 'username'}),
+	    $('<span/>', {text: pathInput.text() + ' ', class: 'path'}),
+	    $('<span/>', {text: userInput.val()}),
+	    $('<p/>', {text: serverResponse}),
+	)
+    );
+}
